7061ddab7adc7be9ebf73ef217f708d76d6114a6
Irfan Mustafa <irfanmustafa2307@gmail.com>
2023-10-19 13:35:13 +0700

    add fitur login google

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "font-awesome/css/font-awesome.min.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterList } from "./routes/RouterList";
 import { ToastContainer } from "react-toastify";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>;
 
@@ -14,8 +15,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ToastContainer/>
-      <RouterList />
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+        <ToastContainer />
+        <RouterList />
+      </GoogleOAuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/utils/api-endpoint.js
+++ src/utils/api-endpoint.js
@@ -5,6 +5,7 @@ export const API_ENDPOINT = {
   DETAIL: "/3/movie",
   REGISTER_USER: "/api/v1/auth/register",
   LOGIN_USER: "/api/v1/auth/login",
+  GOOGLE_OAUTH: "api/v1/auth/google",
   GET_USER: "/api/v1/auth/me",
   MOVIE_POPULAR: "/api/v1/movie/popular",
   DETAIL_MOVIE: "/api/v1/movie",
